ee63c9637351a0596742a271cfb2683854aba328
Daniel Fischer <dfisch15@student.aau.dk>
2017-10-24 21:12:39 +0200

    Wireless communication Transmitter and Receiver

---
+++ receivertest1.ino
@@ -0,0 +1,37 @@
+// ask_receiver.pde
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to receive messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) receiver with an Rx-B1 module
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK Radio(2000,2);
+
+void setup()
+{
+    Serial.begin(9600);	// Debugging only
+    if (!Radio.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+   uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+   uint8_t buflen = sizeof(buf);
+
+   if  (Radio.recv(buf, &buflen))
+   {
+    int i;
+    String text = "";
+    int j;
+
+  for (int j = 0; j < buflen; j++)
+    {
+      text += (char)buf[j];
+    }
+    Serial.println(text);
+    }
+   
+   }
---
+++ transmittertest1.ino
@@ -0,0 +1,29 @@
+// ask_receiver.pde
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to receive messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) receiver with an Rx-B1 module
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK Radio(2000,"",2);
+
+void setup()
+{
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);	// Debugging only
+    if (!Radio.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+   const char *msg = "Forbindelse!";
+
+   Radio.send((uint8_t*)msg, strlen(msg));
+   Radio.waitPacketSent();
+   digitalWrite(13, HIGH);
+   delay(1000);
+   digitalWrite(13, LOW);
+}
