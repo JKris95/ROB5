cf66def713d67df3349003e6c54d9a5a87e8a9db
JKris95 <jk03071995@gmail.com>
2017-11-23 13:21:18 +0100

    RF Drive on button press
    Transmitter and receiver to drive forward when a button is pressed and turn right on default

---
+++ TurnForward/TurnForward.ino
@@ -0,0 +1,115 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// RF variables
+RF24 radio(8, 3); // CE, CSN
+const byte address[6] = "00001";
+char receivedData;
+
+// Linescanners
+int rearScan = 3;
+int frontScan = 2;
+
+
+// Motor variables
+int in1=4;
+int in2=2;
+int in3=6;
+int in4=7;
+/*define channel enable output pins*/
+int ENA=5;
+int ENB=9;
+
+
+void drivingDirection()
+{
+  
+  
+    if (radio.available())// If the NRF240L01 module received data
+    { 
+      radio.read(&receivedData, sizeof(receivedData)); // Read the data and put it into character array
+      delay(20);
+      //Serial.println("received is f");
+    }
+    else
+    {
+      receivedData = 's';
+      //Serial.println("received is s");
+      //Serial.println(receivedData);
+    }
+      
+  
+}
+
+void detectBoundary
+{
+  
+}
+/*define forward function*/
+void _mForward()
+{ 
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  Serial.println("Forward");
+}
+
+void _mRight()
+{
+  analogWrite(ENA,100);
+  analogWrite(ENB,100);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  Serial.println("Right");
+}
+
+void setup()
+{
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  radio.begin();
+  radio.openReadingPipe(0, address); // Read from address given as 2nd argument
+  radio.setPALevel(RF24_PA_MIN); // Set power level - affects range
+  radio.startListening(); // Define as receiver
+}
+
+void loop()
+{
+ /*
+   if (radio.available()) {   // If the NRF240L01 module received data
+    radio.read(&receivedData, sizeof(receivedData)); // Read the data and put it into character array
+    //delay(10);
+   for(int i; i<15000; i++){
+   
+     while  (radio.available())
+     {    
+      _mForward();
+     i=0;
+     }
+   } */
+
+  drivingDirection();
+  Serial.println(receivedData);
+  
+  if (receivedData == 'f')
+  {
+    _mForward(); 
+  }
+  else
+    _mRight();
+    
+  
+}
